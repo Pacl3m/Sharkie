d6e2f082888e1ca33c9838f98f527a1ef9a36603
Pacl3m <134092889+Pacl3m@users.noreply.github.com>
2023-10-23 11:23:08 +0200

    fine tunning

--- levels/level1.js
+++ levels/level1.js
@@ -47,7 +47,7 @@ function loadLevel1() {
             new Background('img/3. Background/Layers/3.Fondo 1/D1.png', 719 * 2),
             new Background('img/3. Background/Layers/2. Floor/D1.png', 719 * 2),
 
-            new Background('img/3. Background/Legacy/Layers/5. Water/d2.png', 719 * 3),
+            new Background('img/3. Background/Legacy/Layers/5. Water/D2.png', 719 * 3),
             new Background('img/3. Background/Layers/4.Fondo 2/L2.png', 719 * 3),
             new Background('img/3. Background/Layers/3.Fondo 1/D2.png', 719 * 3),
             new Background('img/3. Background/Layers/2. Floor/D2.png', 719 * 3),
